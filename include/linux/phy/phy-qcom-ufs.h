938ce8e3a6cc5638bbaa5ce5073f0e2def6454d5
Erfan Abdi <erfangplus@gmail.com>
2021-11-01 16:12:37 +0330

    phy: Fix Include path to use base tree header

--- include/linux/phy/phy-qcom-ufs.h
+++ include/linux/phy/phy-qcom-ufs.h
@@ -6,7 +6,7 @@
 #ifndef PHY_QCOM_UFS_H_
 #define PHY_QCOM_UFS_H_
 
-#include "phy.h"
+#include <linux/phy/phy.h>
 
 void ufs_qcom_phy_ctrl_rx_linecfg(struct phy *generic_phy, bool ctrl);
 void ufs_qcom_phy_set_tx_lane_enable(struct phy *generic_phy, u32 tx_lanes);
